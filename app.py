7609b713865c22828e38e1b930913c0ecb62710f
Thomas-Kulch <54332517+Thomas-Kulch@users.noreply.github.com>
2024-02-22 12:59:22 -0500

    Added dataframe to app

--- app.py
+++ app.py
@@ -9,30 +9,25 @@ from data import DataCleaner
 df = pd.read_csv('vehicles_us.csv') 
 df = DataCleaner.cleaned_df(df)
 
-'''
-Variables
-'''
+
+##Variables
 
 unique_manufacturer = df['make'].unique()
 unique_condition = df['condition'].unique()
 listed_one_week = df[df['days_listed'] <= 7]
+min_year,max_year = int(df[df['model_year'] > 0]['model_year'].min()), int(df['model_year'].max())
 
-#assign min and max year to functions for the year slider on the app
-min_year,max_year = int(df['model_year'].min()), int(df['model_year'].max())
-
-'''
-Streamlit
-'''
+##Streamlit - Inventory Table
 
 #headers
 st.header('CarMin - Used Car Market')
 st.write('The best used cars on the market!')
 
-#Sselection box for manufacturers
+#selection box for manufacturers
 selection = st.selectbox('Select a Manufacturer', unique_manufacturer)
 
 #slider range
-year_range = st.slider('Model Year Filter',value=(min_year.max_year),min_value=min_year,max_value=max_year)
+year_range = st.slider('Model Year Filter',value=(min_year,max_year),min_value=min_year,max_value=max_year)
 actual_range = list(range(year_range[0],year_range[1]+1))
 
 #recent days listed checkbox
@@ -43,4 +38,33 @@ if recent_listings:
 else:
     displayed_df = df[(df.make == selection) & (df.model_year.isin(actual_range))]
 
-st.table(displayed_df)
+#change the order of the indexes
+new_order = [13,2,1,8,0,3,6,4,5,7,9,10,11,12,14,15]
+reordered_df = displayed_df.iloc[:,new_order]
+
+#renaming dataframe columns to clean it up on the app
+reordered_df.rename(columns={'make':'Make',
+                             'model':'Model',
+                             'model_year':'Model Year',
+                             'price':'Price',
+                             'condition':'Condition',
+                             'odometer':'Odometer',
+                             'cylinders':'Cylinders',
+                             'fuel':'Engine Type',
+                             'transmission':'Transmission',
+                             'type':'Type',
+                             'paint_color':'Paint Color',
+                             'is_4wd':'4WD?',
+                             'date_posted':'Post Date',
+                             'day_listed':'Days Listed',
+                             'year_range':'Era',
+                             'age':'Car Age (Years)'},inplace=True)
+
+#display the dataframe
+st.dataframe(reordered_df.set_index(reordered_df.columns[0]).style.format({'Odometer':'{:,}',
+                                                                           'Model Year':'{}',
+                                                                           'Price':'{:,.2f}'}))
+
+#headers for graphs
+st.header('')
+
