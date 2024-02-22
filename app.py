4d13f66ad9bb17b9af14ca5d75fd10f374ac28d4
Thomas-Kulch <54332517+Thomas-Kulch@users.noreply.github.com>
2024-02-22 12:06:26 -0500

    Added streamlit code

--- app.py
+++ app.py
@@ -5,7 +5,42 @@ import altair as al
 
 from data import DataCleaner
 
+#load the data and clean the data
 df = pd.read_csv('vehicles_us.csv') 
 df = DataCleaner.cleaned_df(df)
-print(df.sample(5))
-df.info()
+
+'''
+Variables
+'''
+
+unique_manufacturer = df['make'].unique()
+unique_condition = df['condition'].unique()
+listed_one_week = df[df['days_listed'] <= 7]
+
+#assign min and max year to functions for the year slider on the app
+min_year,max_year = int(df['model_year'].min()), int(df['model_year'].max())
+
+'''
+Streamlit
+'''
+
+#headers
+st.header('CarMin - Used Car Market')
+st.write('The best used cars on the market!')
+
+#Sselection box for manufacturers
+selection = st.selectbox('Select a Manufacturer', unique_manufacturer)
+
+#slider range
+year_range = st.slider('Model Year Filter',value=(min_year.max_year),min_value=min_year,max_value=max_year)
+actual_range = list(range(year_range[0],year_range[1]+1))
+
+#recent days listed checkbox
+recent_listings = st.checkbox('Show Cars Listed in the Last Week')
+
+if recent_listings:
+    displayed_df = listed_one_week[(listed_one_week.make == selection) & (listed_one_week.model_year.isin(actual_range))]
+else:
+    displayed_df = df[(df.make == selection) & (df.model_year.isin(actual_range))]
+
+st.table(displayed_df)
